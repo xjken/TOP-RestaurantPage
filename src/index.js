17c8b027a23acf3870156c25d0d359024fdcbde5
xjken <kenumit@gmail.com>
2026-03-12 02:15:05 -0700

    create nav logic in index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
-console.log("白痴");
-console.log("傻逼")
+import { loadHome } from "./home.js";
+import { loadAbout } from "./about.js";
+import { loadMenu } from "./menu.js";
+
+const homeBtn = document.querySelector("#home-btn");
+const menuBtn = document.querySelector("#menu-btn");
+const aboutBtn = document.querySelector("#about-btn");
+
+const content = document.querySelector("#content");
+
+const resetContent = () => {
+    content.className = "";
+    content.textContent = "";
+};
+
+homeBtn.addEventListener("click", () => {
+    resetContent();
+    loadHome();
+});
+
+menuBtn.addEventListener("click", () => {
+    resetContent();
+    loadMenu();
+});
+
+aboutBtn.addEventListener("click", () => {
+    resetContent();
+    loadAbout();
+});
+
+loadHome();
