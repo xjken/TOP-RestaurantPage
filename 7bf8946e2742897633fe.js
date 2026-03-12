0fd15ebc197bb80680f41154055d2e283a6123c5
xjken <kenumit@gmail.com>
2026-03-12 03:21:47 -0700

    Deployment commit

---
+++ 7bf8946e2742897633fe.js
@@ -0,0 +1,32 @@
+import { loadHome } from "./home.js";
+import { loadAbout } from "./about.js";
+import { loadMenu } from "./menu.js";
+import "./style.css"
+
+const homeBtn = document.querySelector("#home-btn");
+const menuBtn = document.querySelector("#menu-btn");
+const aboutBtn = document.querySelector("#about-btn");
+
+const content = document.querySelector("#content");
+
+const resetContent = () => {
+    content.className = "";
+    content.textContent = "";
+};
+
+homeBtn.addEventListener("click", () => {
+    resetContent();
+    loadHome();
+});
+
+menuBtn.addEventListener("click", () => {
+    resetContent();
+    loadMenu();
+});
+
+aboutBtn.addEventListener("click", () => {
+    resetContent();
+    loadAbout();
+});
+
+loadHome();
